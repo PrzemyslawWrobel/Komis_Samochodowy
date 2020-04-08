9ac823f40117e171fd5a897b3eb4293fc979c847
PW <przemek.wrobel@poczta.fm>
2020-04-08 14:13:47 +0200

    Dodanie widoku

---
+++ KomisSamochodowy/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KomisSamochodowy.Models;
+using KomisSamochodowy.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KomisSamochodowy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISamochodRepository _samochodRepository;
+
+        public HomeController(ISamochodRepository samochodRepository)
+        {
+            _samochodRepository = samochodRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var samochody = _samochodRepository.PobierzWszystkieSamochody().OrderBy(s => s.Marka);
+            var homeVM = new HomeVM()
+            {
+                Tytul = "Przegląd Samochodów",
+                Samochody = samochody.ToList()
+            };
+
+            return View(homeVM);
+        }
+    }
+}
---
+++ KomisSamochodowy/Models/ISamochodRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KomisSamochodowy.Models
+{
+    public interface ISamochodRepository
+    {
+        IEnumerable<Samochod> PobierzWszystkieSamochody();
+        Samochod PobierzSamochodOId();
+    }
+}
