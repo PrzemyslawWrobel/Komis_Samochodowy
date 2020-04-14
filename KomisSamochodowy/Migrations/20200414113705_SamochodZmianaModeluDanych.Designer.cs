4adecc05942638f223796d0a379f719dfb697f33
PW <przemek.wrobel@poczta.fm>
2020-04-14 13:40:23 +0200

    Uaktualnienie Bazy danych

--- KomisSamochodowy/Migrations/20200414113705_SamochodZmianaModeluDanych.Designer.cs
+++ KomisSamochodowy/Migrations/20200414113705_SamochodZmianaModeluDanych.Designer.cs
@@ -3,14 +3,16 @@ using KomisSamochodowy.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KomisSamochodowy.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200414113705_SamochodZmianaModeluDanych")]
+    partial class SamochodZmianaModeluDanych
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ KomisSamochodowy/Migrations/20200414113705_SamochodZmianaModeluDanych.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KomisSamochodowy.Migrations
+{
+    public partial class SamochodZmianaModeluDanych : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "JestWCentrali",
+                table: "Samochody",
+                newName: "JestWcentrali");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "JestWCentrali",
+                table: "Samochody",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JestWCentrali",
+                table: "Samochody");
+
+            migrationBuilder.RenameColumn(
+                name: "JestWcentrali",
+                table: "Samochody",
+                newName: "JestWCentrali");
+        }
+    }
+}
