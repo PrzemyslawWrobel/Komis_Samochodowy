d53257c2e4a35f874a324f86db3a440141b80efd
PW <przemek.wrobel@poczta.fm>
2020-04-14 13:28:27 +0200

    Dodane sane startowe do bazy

--- KomisSamochodowy/Program.cs
+++ KomisSamochodowy/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using KomisSamochodowy.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace KomisSamochodowy
@@ -14,7 +17,26 @@ namespace KomisSamochodowy
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            // CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();
+                    context.Database.Migrate();
+                    DbInitializer.Seed(context);
+                }
+                catch(Exception ex)
+                {
+                    // kod zapisu błędu do loga
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
