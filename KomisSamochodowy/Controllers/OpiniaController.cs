521545eee256023a0c4e10679af44085087a8523
PW <przemek.wrobel@poczta.fm>
2020-04-16 10:33:41 +0200

    widok opini

---
+++ KomisSamochodowy/Controllers/OpiniaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KomisSamochodowy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KomisSamochodowy.Controllers
+{
+    public class OpiniaController : Controller
+    {
+        private readonly IOpiniaRepository _opiniaRepository;
+        public OpiniaController(IOpiniaRepository opiniaRepository)
+        {
+            _opiniaRepository = opiniaRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
