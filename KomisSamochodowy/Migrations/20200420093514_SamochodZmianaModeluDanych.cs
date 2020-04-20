6cea8a7dfd8b8fa734b3d5e36c773cef56b64456
PW <przemek.wrobel@poczta.fm>
2020-04-20 13:52:20 +0200

    update bazy danych

---
+++ KomisSamochodowy/Migrations/20200420093514_SamochodZmianaModeluDanych.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KomisSamochodowy.Migrations
+{
+    public partial class SamochodZmianaModeluDanych : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
