80aca5613d1de3dea7a8cdd6aad12b0e8fa0aede
PW <przemek.wrobel@poczta.fm>
2020-04-21 13:42:54 +0200

    ...

--- KomisSamochodowy/Controllers/OpiniaController.cs
+++ KomisSamochodowy/Controllers/OpiniaController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KomisSamochodowy.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KomisSamochodowy.Controllers
 {
+    [Authorize]
     public class OpiniaController : Controller
     {
         private readonly IOpiniaRepository _opiniaRepository;
